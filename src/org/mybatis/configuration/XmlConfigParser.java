31c73d77ad0e1fb6874de6f99c6d82cda4b6a894
liyunfei <484471679@qq.com>
2022-07-13 15:07:40 +0800

    解析mapper

--- src/org/mybatis/configuration/XmlConfigParser.java
+++ src/org/mybatis/configuration/XmlConfigParser.java
@@ -5,6 +5,7 @@ import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
 
 //解析mybatis-config.xml和userMapper.xml
@@ -43,8 +44,52 @@ public class XmlConfigParser {
         //解析userMapper.xml
         //得到所有的mapper标签
         List<Element> mapperElementList = document.selectNodes("//mapper");
-
+        HashMap<String,SqlMapper> sqlMappers = new HashMap<>();
+        configuaration.setSqlMappers(sqlMappers);
+        for(Element element : mapperElementList){
+            String mapperFileName = element.attributeValue("resource");
+            parserMapper(mapperFileName);
+        }
         System.out.println(configuaration);
+
+    }
+
+    /**
+     * 解析UserMapper.xml
+     * @param mapperFileName
+     * */
+    private static void parserMapper(String mapperFileName) throws Throwable {
+
+        //得到类加载器
+        ClassLoader classLoader = XmlConfigParser.class.getClassLoader();
+
+        //得到inputStream
+        InputStream inputStream = classLoader.getResourceAsStream(mapperFileName);
+
+        //创建saxreader
+        SAXReader saxReader = new SAXReader();
+        //读xml得到document
+        Document document = saxReader.read(inputStream);
+
+        //找到所有select标签
+        List<Element> selectElementList = document.selectNodes("//select");
+
+        //遍历所有select标签
+        for(Element element : selectElementList){
+            //取到select中的id
+            String id = element.attributeValue("id");
+            //取到sql
+            String sql = element.getText();
+            //取到select中的resultType
+            String resultType = element.attributeValue("resultType");
+            //创建sqlMapper
+            SqlMapper sqlMapper = new SqlMapper();
+            sqlMapper.setId(id);
+            sqlMapper.setResultType(resultType);
+            //放到configuration.sqlMappers中
+            configuaration.getSqlMappers().put(id,sqlMapper);
+
+        }
 
     }
 
