3146ba3b1a77a9a0eb39b478c28d9f1f9b67f7d6
liyunfei <484471679@qq.com>
2022-07-13 14:45:34 +0800

    成功获取配置信息

--- src/com/TestMybatis.java
+++ src/com/TestMybatis.java
@@ -1,7 +1,17 @@
 package com;
 
+import org.mybatis.configuration.XmlConfigParser;
+
+import java.io.InputStream;
+
 public class TestMybatis {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Throwable{
+        //加载mybatis-config.xml
+        ClassLoader classLoader = TestMybatis.class.getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream("mybatis-config.xml");
+        //调用配置模块解析xml
+        XmlConfigParser.parser(inputStream);
+
 
     }
 }
--- src/org/mybatis/configuration/XmlConfigParser.java
+++ src/org/mybatis/configuration/XmlConfigParser.java
@@ -1,11 +1,50 @@
 package org.mybatis.configuration;
 
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
 import java.io.InputStream;
+import java.util.List;
 
 //解析mybatis-config.xml和userMapper.xml
 public class XmlConfigParser {
     public static Configuaration configuaration = new Configuaration();
     public static void parser(InputStream inputStream) throws Throwable{
+
+        SAXReader saxReader = new SAXReader();
+        Document document = saxReader.read(inputStream);
+
+        //读取4个property标签
+        List<Element> elementList = document.selectNodes("//property");
+        for(Element element : elementList){
+            //取name属性的值
+            String valueOfName = element.attributeValue("name");
+
+            //取value属性的值
+            String valueOfValue = element.attributeValue("value");
+            switch (valueOfName){
+                case "driver":
+                    configuaration.setDriver(valueOfValue);
+                    break;
+                case "url":
+                    configuaration.setUrl(valueOfValue);
+                    break;
+                case "username":
+                    configuaration.setUsername(valueOfValue);
+                    break;
+                case "password":
+                    configuaration.setPassword(valueOfValue);
+                    break;
+            }
+
+
+        }
+        //解析userMapper.xml
+        //得到所有的mapper标签
+        List<Element> mapperElementList = document.selectNodes("//mapper");
+
+        System.out.println(configuaration);
 
     }
 
